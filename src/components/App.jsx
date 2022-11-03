f4f5eb2781d6551810483606148db031fc22b4fd
Sandra Jimenez <lacakecontact@gmail.com>
2022-11-03 07:25:41 -0700

    added files

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import Set from "./Set";
+import Footer from "./Footer";
+
+function App() {
+    return ( 
+    <div> 
+    <Header/>
+    <Set/>
+    <Footer/> 
+    </div>);
+    }
+    export default App;
